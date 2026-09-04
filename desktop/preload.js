e7fe799cd4ebb99793ca89c800b84138ac0758ec
Deepanshu Kumar Pandit <deepanshukumarpandit2024@gmail.com>
2026-09-04 19:07:31 +0530

    fix: load Electron from root dependency

--- desktop/preload.js
+++ desktop/preload.js
@@ -1,6 +1,5 @@
-const path = require('path');
-module.paths.unshift(path.join(__dirname, '..', 'runtime', 'node_modules'));
 const { contextBridge, ipcRenderer } = require('electron');
+
 contextBridge.exposeInMainWorld('kritamDesktop', {
   getNews: () => ipcRenderer.invoke('news:get'),
   openUrl: (url) => ipcRenderer.invoke('app:open-url', url),
