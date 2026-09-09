b3b656a7d5b9e0f706c00cf7211629ac56cc66b9
Deepanshu Kumar Pandit <deepanshukumarpandit2024@gmail.com>
2026-09-09 14:59:08 +0530

    feat: expose screen capture to renderer

--- desktop/preload.js
+++ desktop/preload.js
@@ -16,6 +16,7 @@ contextBridge.exposeInMainWorld('kritamDesktop', {
   getMemoryPreferences: () => ipcRenderer.invoke('memory:get-preferences'),
   setMemoryPreference: (key, value) => ipcRenderer.invoke('memory:set-preference', key, value),
   clearMemory: () => ipcRenderer.invoke('memory:clear'),
+  captureScreen: () => ipcRenderer.invoke('screen:capture'),
   onCompanionState: (handler) => ipcRenderer.on('companion:state', (_event, payload) => handler(payload)),
   onDailyBriefing: (handler) => ipcRenderer.on('daily-briefing-request', handler)
 });
