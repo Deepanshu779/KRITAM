de7e86681699dbaff1fd0313f220fe124a909c93
Deepanshu Kumar Pandit <deepanshukumarpandit2024@gmail.com>
2026-09-09 15:21:08 +0530

    Harden controlled keyboard input tests

--- core/computer-input.test.js
+++ core/computer-input.test.js
@@ -1,6 +1,6 @@
 const test = require('node:test');
 const assert = require('node:assert/strict');
-const { validatePoint, validateText } = require('./computer-input');
+const { validatePoint, validateText, escapeSendKeys } = require('./computer-input');
 
 test('accepts safe integer mouse coordinates', () => {
   assert.deepEqual(validatePoint(120, 240), { x: 120, y: 240 });
@@ -16,4 +16,8 @@ test('bounds text input and rejects null bytes', () => {
   assert.throws(() => validateText(''), /Text is required/);
   assert.throws(() => validateText('a\0b'), /Invalid text/);
   assert.throws(() => validateText('x'.repeat(2001)), /too long/);
+});
+
+test('escapes SendKeys metacharacters literally', () => {
+  assert.equal(escapeSendKeys('a+b^c%d~e(f)g{h}'), 'a{+}b{^}c{%}d{~}e{(}f{)}g{{}h{}}');
 });
