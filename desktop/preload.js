b4e1f9f1b22d45c6eedf10e1f673fe544aa0b039
Deepanshu Kumar Pandit <deepanshukumarpandit2024@gmail.com>
2026-09-09 14:42:42 +0530

    feat: expose safe tool execution through preload

--- desktop/preload.js
+++ desktop/preload.js
@@ -3,6 +3,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('kritamDesktop', {
   getNews: () => ipcRenderer.invoke('news:get'),
   openUrl: (url) => ipcRenderer.invoke('app:open-url', url),
+  executeTool: (request) => ipcRenderer.invoke('tool:execute', request),
   setLaunchAtLogin: (enabled) => ipcRenderer.invoke('login:set-enabled', enabled),
   showCompanion: () => ipcRenderer.invoke('companion:show'),
   setCompanionState: (state, text) => ipcRenderer.invoke('companion:set-state', state, text),
